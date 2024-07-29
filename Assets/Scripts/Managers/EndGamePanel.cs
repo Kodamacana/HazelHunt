40d494560ae02387911f64ba776c8b03aecbf167
Talha Soylu <63050957+Kodamacana@users.noreply.github.com>
2024-07-29 19:21:52 +0300

    Added Panels

---
+++ Assets/Scripts/Managers/EndGamePanel.cs
@@ -0,0 +1,12 @@
+using TMPro;
+using UnityEngine;
+
+public class EndGamePanel : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI txtMasterUsername;
+    [SerializeField] TextMeshProUGUI txtGuestUsername;
+    [SerializeField] TextMeshProUGUI txtEarnedCoins;
+    [SerializeField] TextMeshProUGUI txtEarnedNuts;
+    [SerializeField] TextMeshProUGUI txtScore;
+
+}
