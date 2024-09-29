4ead8523a8b91bf14fd9e50e61b1c938f4c0d79f
Talha Soylu <63050957+Kodamacana@users.noreply.github.com>
2024-09-30 01:05:36 +0300

    Friends matchmaking system v0.2.5

--- Assets/Scripts/Photon/FriendsMatchmakingManager.cs
+++ Assets/Scripts/Photon/FriendsMatchmakingManager.cs
@@ -5,103 +5,103 @@ using UnityEngine;
 
 public class FriendsMatchmakingManager : MonoBehaviourPunCallbacks
 {
-    public static FriendsMatchmakingManager Instance { get; private set; }
-    public const byte InviteEventCode = 1;  // Davet olay kodu
+    //public static FriendsMatchmakingManager Instance { get; private set; }
+    //public const byte InviteEventCode = 1;  // Davet olay kodu
 
-    PhotonView view;
-    private void Awake()
-    {
-        Instance = this;
+    //PhotonView view;
+    //private void Awake()
+    //{
+    //    Instance = this;
 
-        if (view == null)
-            view = GetComponent<PhotonView>();
-    }
-    public void SendInvite(string invitedPlayerName)
-    {
-        if (!PhotonNetwork.InRoom)
-        {
-            // Oda adý daveti gönderenin kullanýcý adý olsun
-            string roomName = PhotonNetwork.NickName;
+    //    if (view == null)
+    //        view = GetComponent<PhotonView>();
+    //}
+    //public void SendInvite(string invitedPlayerName)
+    //{
+    //    if (!PhotonNetwork.InRoom)
+    //    {
+    //        // Oda adý daveti gönderenin kullanýcý adý olsun
+    //        string roomName = PhotonNetwork.NickName;
 
-            // Oda oluþturma ayarlarý
-            RoomOptions roomOptions = new RoomOptions();
-            roomOptions.MaxPlayers = 2;  // Oyun için maksimum 2 oyuncu
-            PhotonNetwork.CreateRoom(roomName, roomOptions, TypedLobby.Default);  // Odayý oluþtur
+    //        // Oda oluþturma ayarlarý
+    //        RoomOptions roomOptions = new RoomOptions();
+    //        roomOptions.MaxPlayers = 2;  // Oyun için maksimum 2 oyuncu
+    //        PhotonNetwork.CreateRoom(roomName, roomOptions, TypedLobby.Default);  // Odayý oluþtur
 
-            // Oda oluþturulduktan sonra daveti gönder (odadayken RaiseEvent kullanýlabilir)
-            object[] content = new object[] { PhotonNetwork.NickName, invitedPlayerName, roomName };
+    //        // Oda oluþturulduktan sonra daveti gönder (odadayken RaiseEvent kullanýlabilir)
+    //        object[] content = new object[] { PhotonNetwork.NickName, invitedPlayerName, roomName };
 
-            // Daveti RaiseEvent ile karþý tarafa gönder
-            PhotonNetwork.RaiseEvent(InviteEventCode, content, RaiseEventOptions.Default, SendOptions.SendUnreliable);
-        }
-        else
-        {
-            Debug.LogWarning("Zaten bir odadasýnýz!");
-        }
-    }
+    //        // Daveti RaiseEvent ile karþý tarafa gönder
+    //        PhotonNetwork.RaiseEvent(InviteEventCode, content, RaiseEventOptions.Default, SendOptions.SendUnreliable);
+    //    }
+    //    else
+    //    {
+    //        Debug.LogWarning("Zaten bir odadasýnýz!");
+    //    }
+    //}
 
 
-    private void ShowInvitePopup(string invitingPlayerName, string roomName)
-    {
-        OnAcceptInvite(roomName);
-        //invitePopup.SetActive(true);  // UI'da popup'ý aktif et
-        //inviteMessageText.text = invitingPlayerName + " seni oyuna davet ediyor!";  // Popup mesajýný ayarla
+    //private void ShowInvitePopup(string invitingPlayerName, string roomName)
+    //{
+    //    OnAcceptInvite(roomName);
+    //    //invitePopup.SetActive(true);  // UI'da popup'ý aktif et
+    //    //inviteMessageText.text = invitingPlayerName + " seni oyuna davet ediyor!";  // Popup mesajýný ayarla
 
-        //acceptButton.onClick.AddListener(() => OnAcceptInvite(roomName));  // Kabul et butonuna týklanýnca OnAcceptInvite çalýþýr
-    }
+    //    //acceptButton.onClick.AddListener(() => OnAcceptInvite(roomName));  // Kabul et butonuna týklanýnca OnAcceptInvite çalýþýr
+    //}
 
-    public void OnAcceptInvite(string roomName)
-    {
-        if (!PhotonNetwork.InRoom)
-        {
-            PhotonNetwork.JoinRoom(roomName);  // Daveti gönderenin odasýna katýl
-        }
-        else
-        {
-            Debug.LogWarning("Zaten bir odadasýnýz!");
-        }
-    }
+    //public void OnAcceptInvite(string roomName)
+    //{
+    //    if (!PhotonNetwork.InRoom)
+    //    {
+    //        PhotonNetwork.JoinRoom(roomName);  // Daveti gönderenin odasýna katýl
+    //    }
+    //    else
+    //    {
+    //        Debug.LogWarning("Zaten bir odadasýnýz!");
+    //    }
+    //}
 
-    private void OnEnable()
-    {
-        PhotonNetwork.NetworkingClient.EventReceived += NetworkingClient_EventReceived;
-    }
-    private void OnDisable()
-    {
-        PhotonNetwork.NetworkingClient.EventReceived -= NetworkingClient_EventReceived;
-    }
+    //private void OnEnable()
+    //{
+    //    PhotonNetwork.NetworkingClient.EventReceived += NetworkingClient_EventReceived;
+    //}
+    //private void OnDisable()
+    //{
+    //    PhotonNetwork.NetworkingClient.EventReceived -= NetworkingClient_EventReceived;
+    //}
 
-    private void NetworkingClient_EventReceived(EventData photonEvent)
-    {
-        byte eventCode = photonEvent.Code;
+    //private void NetworkingClient_EventReceived(EventData photonEvent)
+    //{
+    //    byte eventCode = photonEvent.Code;
 
-        if (eventCode == InviteEventCode)
-        {
-            object[] data = (object[])photonEvent.CustomData;
-            string invitingPlayerName = (string)data[0];  // Daveti gönderen oyuncunun adý
-            string invitedPlayerName = (string)data[1];   // Davet edilen oyuncunun adý
-            string roomName = (string)data[2];            // Davet gönderenin oluþturduðu oda adý
+    //    if (eventCode == InviteEventCode)
+    //    {
+    //        object[] data = (object[])photonEvent.CustomData;
+    //        string invitingPlayerName = (string)data[0];  // Daveti gönderen oyuncunun adý
+    //        string invitedPlayerName = (string)data[1];   // Davet edilen oyuncunun adý
+    //        string roomName = (string)data[2];            // Davet gönderenin oluþturduðu oda adý
 
-            // Eðer bu davet edilen oyuncu bizsek, daveti kabul edip odaya katýlalým
-            if (invitedPlayerName == PhotonNetwork.NickName)
-            {
-                ShowInvitePopup(invitingPlayerName, roomName);  // Popup göster ve odaya katýlma seçeneði sun
-            }
-        }
-    }
+    //        // Eðer bu davet edilen oyuncu bizsek, daveti kabul edip odaya katýlalým
+    //        if (invitedPlayerName == PhotonNetwork.NickName)
+    //        {
+    //            ShowInvitePopup(invitingPlayerName, roomName);  // Popup göster ve odaya katýlma seçeneði sun
+    //        }
+    //    }
+    //}
 
-    public override void OnPlayerEnteredRoom(Player newPlayer)
-    {
-        if (PhotonNetwork.CurrentRoom.PlayerCount == 2)
-        {
-            view.RPC("StartMatch", RpcTarget.All);
-        }
-    }
+    //public override void OnPlayerEnteredRoom(Player newPlayer)
+    //{
+    //    if (PhotonNetwork.CurrentRoom.PlayerCount == 2)
+    //    {
+    //        view.RPC("StartMatch", RpcTarget.All);
+    //    }
+    //}
 
-    [PunRPC]
-    private void StartMatch()
-    {
-        PhotonNetwork.LoadLevel("GameScene");
-    }
+    //[PunRPC]
+    //private void StartMatch()
+    //{
+    //    PhotonNetwork.LoadLevel("GameScene");
+    //}
 
 }
